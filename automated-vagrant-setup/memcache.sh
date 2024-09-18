eaeba070cb2804dc9c71f83196e400122589f1ae
Ahmed Mahmoud <ahmedmahmoud0131@gmail.com>
2024-09-18 07:04:51 +0300

    Edit ansible role files

--- automated-vagrant-setup/memcache.sh
+++ automated-vagrant-setup/memcache.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-#SSH configuration section (Uncomment to enable SSH settings)
-# SSHD_CONFIG="/etc/ssh/sshd_config"
-
-# Check if the script is run as root (Uncomment to enforce root user execution)
-# if [[ $EUID -ne 0 ]]; then
-#    exit 1
-# fi
-
-# Enable PasswordAuthentication in SSH config (Uncomment to modify SSH settings)
-# sed -i '/^#PasswordAuthentication/c\PasswordAuthentication yes' "$SSHD_CONFIG"
-# sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/' "$SSHD_CONFIG"
-
-# Enable PermitRootLogin in SSH config (Uncomment to allow root login via SSH)
-# sed -i '/^#PermitRootLogin/c\PermitRootLogin yes' "$SSHD_CONFIG"
-# sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' "$SSHD_CONFIG"
-
-# Restart SSH service to apply changes (Uncomment to restart SSH)
-# systemctl restart sshd
-
-##################
-
 # Update the system and install required packages
 sudo yum update -y
 sudo dnf install epel-release -y
